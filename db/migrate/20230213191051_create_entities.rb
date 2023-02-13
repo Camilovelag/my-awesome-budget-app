e672cc83a7ac645b6a54488990514cb9bd6d689d
camilovelag <camilo.vela.galindo@gmail.com>
2023-02-13 14:17:11 -0500

    Create DB schema

---
+++ db/migrate/20230213191051_create_entities.rb
@@ -0,0 +1,10 @@
+class CreateEntities < ActiveRecord::Migration[7.0]
+  def change
+    create_table :entities do |t|
+      t.string :name
+      t.integer :amount
+
+      t.timestamps
+    end
+  end
+end
