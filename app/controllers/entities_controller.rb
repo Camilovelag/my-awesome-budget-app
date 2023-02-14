5bc4c0f62afd733fa5888983707eed66e7f32f3a
camilovelag <camilo.vela.galindo@gmail.com>
2023-02-14 16:51:26 -0500

    Add category checkboxes to new transaction form

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -4,13 +4,19 @@ class EntitiesController < ApplicationController
   end
 
   def new
+    @groups = Group.all
     @entity = Entity.new
+    @entity_group = EntityGroup.new
   end
 
   def create
     @entity = Entity.new(entity_params)
     @entity.author_id = current_user.id
+    @groups_array = entity_group_params
     if @entity.save
+      @groups_array.each do |group|
+        Entity_group.create(entity_id: @entity.id, group_id: group.id)
+      end
       redirect_to entities_path, notice: 'Transaction successfully created!'
     else
       render :new
@@ -21,5 +27,9 @@ class EntitiesController < ApplicationController
 
   def entity_params
     params.require(:entity).permit(:name, :amount)
+  end
+
+  def entity_group_params
+    params.require(:entity_group).permit(group_ids: [])
   end
 end
