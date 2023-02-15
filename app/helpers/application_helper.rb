f99769ef264863afb7e2524ed33a89994865db3a
camilovelag <camilo.vela.galindo@gmail.com>
2023-02-15 13:10:37 -0500

    Fix page title bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def page_title
+    case [controller.controller_name, controller.action_name]
+    when %w[groups new]
+      'new category'
+    when %w[groups index]
+      'categories'
+    when %w[entities new]
+      'new transaction'
+    when %w[entities index]
+      'transactions'
+    end
+  end
 end
